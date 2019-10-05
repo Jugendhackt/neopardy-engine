9569b40e71ec007d5d15c38a6d93b91c0fc91648
Moritz Fromm <git@moritz-fromm.de>
2019-10-05 11:53:03 +0200

    Rename crud controllers to admin controllers

--- src/Controller/AdminGameController.php
+++ src/Controller/AdminGameController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/game")
+ * @Route("/admin/game")
  */
 class GameController extends AbstractController
 {
--- src/Controller/AdminPlayerController.php
+++ src/Controller/AdminPlayerController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/player")
+ * @Route("/admin/player")
  */
 class PlayerController extends AbstractController
 {
