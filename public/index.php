b0c4d6550244fe0acd724d29fa17f9e156465454
Hendrik <hendrik-buerger@web.de>
2019-10-06 00:49:06 +0200

    Begin of homepage

--- public/index.php
+++ public/index.php
@@ -3,12 +3,11 @@
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
-
 require dirname(__DIR__).'/config/bootstrap.php';
+
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
-
     Debug::enable();
 }
 
